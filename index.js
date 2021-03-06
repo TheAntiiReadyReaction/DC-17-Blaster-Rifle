3953d7fc131cab70d7ad2331031b0dbbe08918dc
TheAntiiReadyReaction <80123698+TheAntiiReadyReaction@users.noreply.github.com>
2021-03-05 20:07:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ function createShell () {
       const src_iframe = document.querySelector('[title="Optimizely Internal Frame"]');
       const src_iframe_new_page = null;
       
-      src_iframe.contentWindow.document.getElementById("create-page-dialog__title").value = String(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
-      document.getElementByClassName("wds-button wds-is-text create-page-dialog__button").click();
+      setTimeout(function () {
+        document.getElementById("create-page-dialog__title").value = String(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+        document.getElementByClassName("wds-button wds-is-text create-page-dialog__button").click();
+      }, 1000);
     }
   }
 }
